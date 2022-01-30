114830e47dc3635b0b6ae86ab85b0e68c1b6ed09
stephane888 <kksasteph888@gmail.com>
2022-01-30 06:49:18 +0100

    MAJ

--- src/ApiRest/Shopify.php
+++ src/ApiRest/Shopify.php
@@ -1,55 +1,52 @@
 <?php
+
 namespace Stephane888\WbuShopify\ApiRest;
 
 class Shopify extends CurlShopify {
-
   protected $api_version = '2020-07';
-
   protected $has_error = false;
-
+  
   /**
    * Return la premiere erreur rencontrer.
    *
    * @var string
    */
   protected $error_msg = '';
-
-  function __construct($configs)
-  {
+  
+  function __construct($configs) {
     parent::__construct($configs);
   }
-
+  
   /**
    * definit la version d'api.
    */
-  public function setApiVersion($value)
-  {
+  public function setApiVersion($value) {
     $this->api_version = $value;
   }
-
+  
+  public function get() {
+    return $this->GetDatas();
+  }
+  
   /**
    * Permet de determiner s'il ya une erreur;
    */
-  protected function ValidResult($result)
-  {
-    if (! empty($result['errors'])) {
+  protected function ValidResult($result) {
+    if (!empty($result['errors'])) {
       $this->has_error = true;
       $this->error_msg = $this->getErrorString($result['errors']);
     }
   }
-
-  public function checkHasError()
-  {
+  
+  public function checkHasError() {
     return $this->has_error;
   }
-
-  public function getErrorMsg()
-  {
+  
+  public function getErrorMsg() {
     return $this->error_msg;
   }
-
-  private function getErrorString($errors)
-  {
+  
+  private function getErrorString($errors) {
     if (\is_array($errors)) {
       $errors = reset($errors);
       if (\is_array($errors)) {
@@ -57,8 +54,10 @@ class Shopify extends CurlShopify {
         $this->getErrorString($errors);
       }
       return $errors;
-    } else {
+    }
+    else {
       return $errors;
     }
   }
+  
 }
