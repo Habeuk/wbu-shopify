c235d05487f6205a2f0358aa83502426c8af6182
stephane888 <kksasteph888@gmail.com>
2022-02-21 10:27:47 +0100

    Ajout de la function get à CurlShopify

--- src/ApiRest/CurlShopify.php
+++ src/ApiRest/CurlShopify.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Stephane888\WbuShopify\ApiRest;
 
 use Stephane888\Debug\debugLog;
@@ -6,49 +7,42 @@ use Stephane888\Debug\debugLog;
 /**
  *
  * @author stephane
- *
+ *        
  */
 class CurlShopify {
-
   public $key_api = null;
-
   protected $last_response_headers = null;
-
   public $path = "";
-
   public $showHeader = 0;
-
   public $api_full_url = null;
-
   protected $http_code = null;
-
-  function __construct($configs)
-  {
-    if (! empty($configs['api_key']) && ! empty($configs['shop_domain']) && ! empty($configs['secret'])) {
+  
+  function __construct($configs) {
+    if (!empty($configs['api_key']) && !empty($configs['shop_domain']) && !empty($configs['secret'])) {
       $this->api_key = $configs['api_key'];
       $this->shop_domain = $configs['shop_domain'];
       $this->secret = $configs['secret'];
-    } else {
+    }
+    else {
       $this->buildError("Configuration non valide, vous definir: 'api_key','shop_domain','secret','webhook_key' ", 401, []);
     }
   }
-
+  
   /**
    *
    * @param
-   *          $arg
+   *        $arg
    * @return mixed
    */
-  protected function PostDatas($arg)
-  {
-
+  protected function PostDatas($arg) {
+    
     // $api_url = $this->db_api->protocole.'://'.$this->db_api->ndd.'/'.$this->path;
     $api_url = 'https://' . $this->shop_domain . '/' . $this->path;
     $headers = array(
       "Content-Type: application/json; charset=utf-8",
       'Expect:'
     );
-
+    
     $curl = curl_init($api_url);
     // curl_setopt($curl, CURLOPT_HEADER, 1);
     curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2);
@@ -64,11 +58,10 @@ class CurlShopify {
     // echo '<pre>'; var_dump(json_decode($result)); echo '</pre>';
     return $result;
   }
-
+  
   /**
    */
-  public function GetDatas()
-  {
+  public function GetDatas() {
     // $url=$this->db_api->protocole.'://'.$this->db_api->ndd.'/'.$this->path;
     $url = 'https://' . $this->shop_domain . '/' . $this->path;
     $headers = array(
@@ -91,17 +84,20 @@ class CurlShopify {
     curl_close($ch);
     return $result;
   }
-
+  
+  public function get() {
+    return $this->GetDatas();
+  }
+  
   /**
    */
-  public function PutDatas($arg)
-  {
+  public function PutDatas($arg) {
     $url = 'https://' . $this->shop_domain . '/' . $this->path;
     $headers = array(
       "Content-Type: application/json; charset=utf-8",
       'Expect:'
     );
-
+    
     $ch = curl_init();
     // curl_setopt($ch, CURLOPT_HEADER,$this->showHeader);
     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
@@ -118,11 +114,10 @@ class CurlShopify {
     curl_close($ch);
     return $result;
   }
-
+  
   /**
    */
-  public function DeleteDatas()
-  {
+  public function DeleteDatas() {
     $api_url = $this->api_full_url = 'https://' . $this->shop_domain . $this->path;
     $headers = array(
       "Content-Type: application/json; charset=utf-8",
@@ -138,18 +133,17 @@ class CurlShopify {
     // curl_setopt($curl, CURLOPT_POSTFIELDS, $arg);
     curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'DELETE');
     curl_setopt($curl, CURLOPT_USERPWD, $this->api_key . ':' . $this->secret); // API KEY
-
+    
     $result = curl_exec($curl);
     $this->http_code = curl_getinfo($curl, CURLINFO_HTTP_CODE);
     curl_close($curl);
     return $result;
   }
-
-  public function get_http_code()
-  {
+  
+  public function get_http_code() {
     return $this->http_code;
   }
-
+  
   /**
    * Getionnaire d'erreur de logique.
    *
@@ -157,8 +151,7 @@ class CurlShopify {
    * @param int $code
    * @param string|array $error
    */
-  public function buildError($title, $code, $error)
-  {
+  public function buildError($title, $code, $error) {
     $filename = 'CurlShopify_debug_' . date('m-Y');
     $data = [
       'title' => $title,
@@ -171,4 +164,5 @@ class CurlShopify {
     // $error = json_encod
     die('BGQB###' . json_encode($error) . 'ENDQB###');
   }
+  
 }
