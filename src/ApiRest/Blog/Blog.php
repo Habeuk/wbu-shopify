9df5fcbee4ca3f117a1dce643701f27d84af353e
kouwa stephane <kksasteph888@gmail.com>
2024-04-04 10:54:19 +0200

    update data

--- src/ApiRest/Blog/Blog.php
+++ src/ApiRest/Blog/Blog.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Stephane888\WbuShopify\ApiRest\Blog;
 
 use Stephane888\WbuShopify\ApiRest\Shopify;
@@ -6,47 +7,44 @@ use Stephane888\WbuShopify\ApiRest\Metafields\MetafieldsTrait;
 
 class Blog extends Shopify {
   use MetafieldsTrait;
-
-  function __construct($configs)
-  {
+  
+  function __construct($configs) {
     parent::__construct($configs);
   }
-
+  
   /**
    * Permet de recuperer les blogs.
    */
-  public function getBlogs($id_blog = null)
-  {
+  public function getBlogs($id_blog = null) {
     $this->path = 'admin/api/' . $this->api_version . '/blogs.json';
-    if (! empty($id_blog)) {
+    if (!empty($id_blog)) {
       $this->path = 'admin/api/' . $this->api_version . '/blogs/' . $id_blog . '.json';
     }
     $datas = $this->GetDatas();
     return json_decode($datas, true);
   }
-
+  
   /**
    *
    * @param integer $id_blog
    * @return mixed
    */
-  public function getMetafields($id_blog)
-  {
+  public function getMetafields($id_blog) {
     $this->path = 'admin/api/' . $this->api_version . '/blogs/' . $id_blog . '/metafields.json';
     return $this->LoadMetafiels();
   }
-
+  
   /**
    *
    * @param integer $id_blog
    * @return mixed
    */
-  public function getBlogsWithMetafields($id_blog = null)
-  {
+  public function getBlogsWithMetafields($id_blog = null) {
     $blogs = $this->getBlogs($id_blog);
     foreach ($blogs as $key => $blog) {
       $blogs[$key]['metafields'] = $this->getMetafields($blog['id']);
     }
     return $blogs;
   }
+  
 }
