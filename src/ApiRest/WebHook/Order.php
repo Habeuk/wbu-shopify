2ade9ba630d79df5f8b6c58167fdfb244ae310a0
kouwa stephane <kksasteph888@gmail.com>
2025-11-26 12:06:13 +0100

    add methode get idOder

--- src/ApiRest/WebHook/Order.php
+++ src/ApiRest/WebHook/Order.php
@@ -1,17 +1,19 @@
 <?php
+
 namespace Stephane888\WbuShopify\ApiRest\WebHook;
 
 use Stephane888\WbuShopify\ApiRest\WebHook\Traits\Order as TraitOrder;
 
 class Order extends WebHook {
   use TraitOrder;
-
   protected $order;
-
   protected $id_order;
 
-  function __construct($SHOPIFY_APP_SECRET = null, $entity = null)
-  {
+  function __construct($SHOPIFY_APP_SECRET = null, $entity = null) {
     parent::__construct($SHOPIFY_APP_SECRET, $entity);
+  }
+
+  public function getIdOrder() {
+    return $this->id_order;
   }
 }
